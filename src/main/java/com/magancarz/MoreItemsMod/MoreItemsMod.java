d7027011d6feac8ae759ce56b77bd77b4fcc7383
Mateusz Gancarz <grzmateusz6@gmail.com>
2023-01-15 19:31:30 +0100

    TEMP: moving unfinished feature to another PC

--- src/main/java/com/magancarz/MoreItemsMod/MoreItemsMod.java
+++ src/main/java/com/magancarz/MoreItemsMod/MoreItemsMod.java
@@ -1,14 +1,12 @@
 package com.magancarz.MoreItemsMod;
 
 import com.magancarz.MoreItemsMod.items.ModItems;
+import com.magancarz.MoreItemsMod.blocks.ModBlocks;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,9 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -35,7 +31,8 @@ public class MoreItemsMod {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
         // Register the Deferred Register to the mod event bus so items get registered
-        ModItems.ITEMS.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/com/magancarz/MoreItemsMod/items/ModItems.java
+++ src/main/java/com/magancarz/MoreItemsMod/items/ModItems.java
@@ -4,6 +4,7 @@ import com.magancarz.MoreItemsMod.MoreItemsMod;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -16,7 +17,12 @@ public class ModItems {
     // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties()));
 
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
     public static void addCreative(CreativeModeTabEvent.BuildContents event) {
+        List<ITEMS.getEntries()
         if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES)
             event.accept(ModItems.ZIRCON);
     }
